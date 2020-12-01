9552635e1e85d3e24335482ad05118a5452bd21b
iamdeve <wajdanaeli@gmail.com>
2020-12-01 20:58:50 +0500

    2nd step added

--- src/components/HomeForm/HomeForm.js
+++ src/components/HomeForm/HomeForm.js
@@ -10,7 +10,14 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import SearchIcon from '@material-ui/icons/Search';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import { Card, CardContent, FormControl, Input, InputLabel, InputAdornment, IconButton } from '@material-ui/core';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { Card, CardContent, FormControl, Input, InputLabel, InputAdornment, IconButton, TextField } from '@material-ui/core';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import NY from '../../assets/ny.png';
+import MIAMI from '../../assets/miami.png';
+import TH from '../../assets/thailand.png';
 
 const ColorlibConnector = withStyles({
 	alternativeLabel: {
@@ -59,6 +66,9 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		width: '50%',
 		margin: '3rem auto',
+		'@media screen and (max-width:787px)': {
+			width: '100%',
+		},
 	},
 	button: {
 		marginRight: theme.spacing(1),
@@ -66,6 +76,97 @@ const useStyles = makeStyles((theme) => ({
 	instructions: {
 		marginTop: '0 !important',
 		marginBottom: theme.spacing(1),
+	},
+	stepContainer: {
+		margin: '3rem 0',
+	},
+	step1: {
+		color: '#fff',
+	},
+	step: {
+		color: '#fff',
+		background: '#F36888',
+		width: '30px',
+		height: '30px',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: '1rem auto',
+		borderRadius: '50%',
+		fontWeight: 'bold',
+	},
+	mainCard: {
+		width: '60%',
+		margin: 'auto',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		'@media screen and (max-width:787px)': {
+			width: '100%',
+		},
+	},
+	PR0: {
+		marginBottom: '1rem',
+		width: '50%',
+		float: 'left',
+		'@media screen and (max-width:787px)': {
+			paddingRight: '15px',
+		},
+	},
+	city: {
+		position: 'relative',
+		display: 'flex',
+		width: '120px',
+		height: '32px',
+		margin: '.5rem 0',
+	},
+	cityImg: {
+		display: 'block',
+		width: '100%',
+		height: '100%',
+		borderRadius: '.3rem',
+	},
+	cityname: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		background: 'rgba(0,0,0,0.3)',
+		fontSize: '14px',
+		color: '#fff',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: '.3rem',
+	},
+	cross: {
+		position: 'absolute',
+		top: '-10px',
+		right: '-8px',
+		height: '15px',
+		width: '15px',
+		borderRadius: '50%',
+		background: '#C4C4C4',
+		display: 'flex',
+		fontSize: '10px',
+		cursor: 'pointer',
+		justifyContent: 'center',
+		textTransform: 'capitalize',
+	},
+	arrowRight: {
+		height: '100%',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	dates: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		'& span':{
+			margin:'0 1rem'
+		}
 	},
 }));
 
@@ -90,11 +191,16 @@ function getSteps(activeStep) {
 	];
 }
 
+const cities = [
+	{ name: 'New York', img: NY },
+	{ name: 'Miami', img: MIAMI },
+	{ name: 'Thailand', img: TH },
+	{ name: 'New York', img: NY },
+];
 function HomeForm() {
 	const classes = useStyles();
 	const [activeStep, setActiveStep] = React.useState(0);
 	const [steps, setSteps] = React.useState(getSteps(activeStep));
-
 	const handleNext = () => {
 		setActiveStep((prevActiveStep) => prevActiveStep + 1);
 		setSteps(getSteps(activeStep + 1));
@@ -111,18 +217,18 @@ function HomeForm() {
 	};
 
 	const stepContent = [
-		<>
+		<div className={classes.step1}>
 			<div className={classes.step}>{activeStep + 1}</div>
 			<h4>First thing first</h4>
 			<p>Start planning your dream trip</p>
 
-			<Card style={{ width: '50%', margin: 'auto' }}>
+			<Card className={classes.mainCard}>
 				<CardContent>
 					<FormControl style={{ width: '100%' }}>
 						<InputLabel htmlFor='standard-adornment-password'>Trip name</InputLabel>
 						<Input />
 					</FormControl>
-					<FormControl>
+					<FormControl style={{ width: '100%' }}>
 						<InputLabel htmlFor='standard-adornment-password'>Destination(s)</InputLabel>
 						<Input
 							endAdornment={
@@ -134,26 +240,55 @@ function HomeForm() {
 							}
 						/>
 					</FormControl>
+					<div style={{ margin: '.5rem 0' }} className={classes.cities}>
+						{cities.map((city, index) => (
+							<div className={classes.PR0} key={index}>
+								<div className={classes.city}>
+									<div className={classes.cityname}>{city.name}</div>
+									<div className={classes.cross}>x</div>
+									<img className={classes.cityImg} src={city.img} alt={city.name} />
+									<div className={classes.arrowRight}>
+										<ArrowRightAltIcon />
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
 					<FormControl style={{ width: '100%' }}>
 						<InputLabel htmlFor='standard-adornment-password'>Total Budget</InputLabel>
 						<Input />
 					</FormControl>
 				</CardContent>
 			</Card>
-		</>,
+		</div>,
+		<div className={classes.step1}>
+			<div className={classes.step}>{activeStep + 1}</div>
+			<h4>The Dates</h4>
+			<p>Choose when you want to go</p>
+
+			<Card style={{ height: '250px' }} className={classes.mainCard}>
+				<CardContent>
+					<div className={classes.dates}>
+						<TextField type='text' label='Start Date' />
+						<span>to</span>
+						<TextField type='text' label='End Date' />
+					</div>
+				</CardContent>
+			</Card>
+		</div>,
 	];
 	return (
 		<div className={classes.root}>
 			<Stepper style={{ padding: 0, position: 'relative', background: 'transparent' }} alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
-				{steps.map((label) => (
-					<Step key={label}>
+				{steps.map((label, index) => (
+					<Step key={index}>
 						<StepLabel style={{ marginTop: 0 }} StepIconComponent={ColorlibStepIcon}>
 							{label}
 						</StepLabel>
 					</Step>
 				))}
 			</Stepper>
-			<div>
+			<div className={classes.stepContainer}>
 				{activeStep === steps.length ? (
 					<div>
 						<Typography className={classes.instructions}>All steps completed - you&apos;re finished</Typography>
@@ -163,10 +298,16 @@ function HomeForm() {
 					</div>
 				) : (
 					<div style={{ textAlign: 'center' }}>
-						<Typography className={classes.instructions}>{stepContent[activeStep]}</Typography>
+						<div className={classes.instructions}>{stepContent[activeStep]}</div>
 						<div>
 							<Button disabled={activeStep === 0} onClick={handleBack} className={clsx(classes.button, 'btn-transparent')}>
-								Back
+								{activeStep > 0 ? (
+									<>
+										<ChevronLeftIcon /> Back
+									</>
+								) : (
+									<>Cancel</>
+								)}
 							</Button>
 							<Button variant='contained' color='primary' onClick={handleNext} className={clsx(classes.button, 'btn-filled')}>
 								{activeStep === steps.length - 1 ? (
